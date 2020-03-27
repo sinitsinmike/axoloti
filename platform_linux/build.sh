75a80491d1247b295405592d554bbf1cee881296
Pascal Huerst <pascal.huerst@gmail.com>
2020-03-27 20:51:26 +0100

    build.sh: Add p7zip-plugins to dependencies for fedora
    ChibiOS seems to be in need of 7z. This installs the necessary
    package for fedora.

--- platform_linux/build.sh
+++ platform_linux/build.sh
@@ -72,7 +72,7 @@ case $OS in
         echo "detected Fedora"
         sudo dnf group install "Development Tools"
         sudo dnf -y install libusb dfu-util libtool libudev-devel automake autoconf \
-        ant curl bzip2
+        ant curl bzip2 p7zip-plugins
         ;;
     *)
         echo "Cannot handle dist: $OS"
